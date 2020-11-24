7bd269b05bc9a19f7d94e250cb0e069783dd83ec
JoseEduardoSalles <jose.salles@bandtec.com.br>
2020-11-24 12:23:23 -0300

    projeto individual

---
+++ projeto-site/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  production: {
+    username: 'Jose_Eduardo',
+    password: 'Bernadete1',
+    database: 'usuario ',
+    host: 'sitepessoal.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  }
+};
+ 
